204a1fbaa3011dde4e44250298eaa60860b274ed
Ella Usik <usik.ella@gmail.com>
2019-03-26 11:23:05 +0300

    create menu object

---
+++ scripts/menu.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var objectMenu = {
+  menuSection: {
+    sectionName: 'Coffee',
+    sectionPriseList: {
+      priseListPosition: {
+        positionName: 'Latte',
+        positionCost: '$3.00'
+      },
+      priseListPosition: {
+        positionName: 'Mocha',
+        positionCost: '$3.25'
+      },
+      priseListPosition: {
+        positionName: 'Expresso',
+        positionCost: '$3.75'
+      },
+      priseListPosition: {
+        positionName: 'Capuccino',
+        positionCost: '$3.00'
+      }
+    }
+  },
+  menuSection: {
+    sectionName: 'Tea',
+    sectionPriseList: {
+      priseListPosition: {
+        positionName: 'Iced tea',
+        positionCost: '$2.75'
+      },
+      priseListPosition: {
+        positionName: 'Cold brew',
+        positionCost: '$3.25'
+      },
+      priseListPosition: {
+        positionName: 'Jasmine green',
+        positionCost: '$3.75'
+      },
+      priseListPosition: {
+        positionName: 'Peppermint Herbal',
+        positionCost: '$3.00'
+      }
+    }
+  },
+  menuSection: {
+    sectionName: 'Desert',
+    sectionPriseList: {
+      priseListPosition: {
+        positionName: 'Tiramisu',
+        positionCost: '$3.00'
+      },
+      priseListPosition: {
+        positionName: 'Cheese cake',
+        positionCost: '$3.25'
+      },
+      priseListPosition: {
+        positionName: 'Black forest',
+        positionCost: '$3.75'
+      },
+      priseListPosition: {
+        positionName: 'Brownie',
+        positionCost: '$3.00'
+      }
+    }
+  }
+};
